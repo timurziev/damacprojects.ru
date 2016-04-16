88b5383644606406fb60a8a6596a2837e61370c7
Ahmed <one_day1@mail.ru>
2016-04-16 16:05:30 +0300

    check

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}">
