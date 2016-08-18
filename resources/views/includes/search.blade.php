844b907a1a513539a4bebc7b284026c6acc9279c
teslaAi <one_day1@mail.ru>
2016-08-18 15:31:34 +0300

    Update search.blade.php

--- resources/views/includes/search.blade.php
+++ resources/views/includes/search.blade.php
@@ -11,26 +11,16 @@
 				<div class="select-arrow"></div>
 			</div>
 			<div class="select-wrapper">
-<<<<<<< HEAD
-				<select class="drop-list" name="city">
-					<option disabled selected>Выбор страны</option>	
-					@foreach ($locations as $location)
-						<option value="{{ $location->id }}"
-						@if ($location->id == Request::input('city') )
-=======
 				<select class="drop-list" name="country">
 					<option disabled selected>Выберите страну</option>	
 					@foreach ($countries as $country)
 						<option value="{{ $country->id }}"
 						@if ($country->id == Request::input('country') )
->>>>>>> origin/master
 							selected
 						@endif
-
 							>{{ $country->name }}</option>
 
 							>{!! $country->name !!}</option>
-
 					@endforeach	
 				</select>
 				<div class="select-arrow"></div>
