f38b6cc4912f6a5c50488158b640f85b8e25101b
Ismail <ismail.albakov@gmail.com>
2016-10-03 16:05:15 +0300

    Points: 2, 3, 16 (info block height).

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,42 @@
 // masonry init in projects search results grid view
 var masonryInit = function() {
 	if($(document).width() > 1199 && $('.grid-view').length) {
-		$('.grid-view').masonry({
-			columnWidth: 100,
-			itemSelector: '.grid-item',
-			transitionDuration: 0
-		});
+		setTimeout(function() {
+			$('.grid-view').masonry({
+				columnWidth: 100,
+				itemSelector: '.grid-item',
+				transitionDuration: 0
+			});
+		}, 500);
 	}
 };
 
 // set slider and image height
 var sliderAutoHeight = function() {
 	if($(".slider").length && $(document).width() > 1199) {
-		$(".slider").attr("style", "max-height: "+$(window).height()+"px");
+		$(".slider").attr("style", "height: "+$(window).height()+"px!important;");
 		$(".slider img").attr("style", "height: "+$(window).height()+"px");
+	} else if($(".slider").length && $(document).width() < 1199) {
+		$(".slider").removeAttr("style");
+		$(".slider img").removeAttr("style");
+	}
+};
+
+// set info block height in grid view
+var itemGridAutoHeight = function() {
+	if($(".grid-view .info-block").length > 1 && $(document).width() > 1199) {
+
+		var itemMaxHeight = 0;
+
+		$(".grid-view .info-block").each(function(i) {
+			if(itemMaxHeight < $(this).outerHeight()) {
+				itemMaxHeight = $(this).outerHeight();
+			}
+		});
+
+		if(itemMaxHeight > 0) {
+			$(".grid-view .info-block").attr("style", "height: "+itemMaxHeight+"px;");
+		}
 	}
 };
 
@@ -146,6 +169,9 @@ $(document).ready(function() {
     if($(".filter").length && $(document).scrollTop() > $(window).height()+$(".filter").outerHeight()) {
     	$(".filter").addClass("scrolled");
     }
+
+    // set item height in grid view
+	itemGridAutoHeight();
 });
 
 $(window).resize(function() {
@@ -159,7 +185,7 @@ $(window).resize(function() {
 $(window).scroll(function() {
 
 	// scroll to filter on home page (1 wheel from top)
-	if($(".filter").length && $(document).width() > 1199) {
+	if($(".slider").length && $(".filter").length && $(document).width() > 1199) {
 		$(document).on('mousewheel', function(e) {
 	        if(!$(".filter").hasClass("scrolled") && e.originalEvent.wheelDeltaY < 0) {
 	        	$('html, body').animate({
