c841f338531ce804886dd8b5f593c7707ae1c950
teslaAi <storona77@gmail.com>
2016-11-07 18:31:48 +0300

    fixing a gallery

--- resources/views/includes/photo_item.blade.php
+++ resources/views/includes/photo_item.blade.php
@@ -2,8 +2,8 @@
 	<p>Наслаждайтесь прекрасными фотографиями наших проектов</p>
 		<div class="images">
 			<ul>
-				@foreach($images as $image)
-					<li><a href="{{ Request::root() }}/uploads/projects/small/{{ $image->name }}" data-lightbox="roadtrip"><img src="{{ Request::root() }}/uploads/projects/small/{{ $image->name }}" alt=""></a></li>
+				@foreach($images as $key => $image)
+					<li class="{{ ($key+1) % 3 == 0 ? 'last' : ''}}"><a href="{{ Request::root() }}/uploads/projects/small/{{ $image->name }}" data-lightbox="roadtrip"><img src="{{ Request::root() }}/uploads/projects/small/{{ $image->name }}" alt=""></a></li>
 				@endforeach
 			</ul>
 		</div>
