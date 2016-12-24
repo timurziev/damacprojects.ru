18487817a88dfaff35aa1821cb2162fbf33a4a3f
teslaAi <one_day1@mail.ru>
2016-12-24 20:06:21 +0300

    delete callback widget

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -163,10 +163,6 @@
 		(function(){ var widget_id = 'gXd3fPk2YB';var d=document;var w=window;function l(){
 		var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 		<!-- {/literal} END JIVOSITE CODE -->
-
-		<script type="text/javascript"
-            src="//cdn.callbackhunter.com/cbh.js?hunter_code=cc65d82faf6afe1ea6cc1bba94e4b4c7" charset="UTF-8">
-        </script>
 	</footer>
 	<div class="popup">
 		<div class="popup-manager">
