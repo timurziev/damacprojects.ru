e364157c1910e12e947fc9b8642e32bbe94f08da
teslaAi <storona77@gmail.com>
2016-12-24 21:04:39 +0300

    about page fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -53,8 +53,7 @@
 				<div class="section-title">
 					<h2>Наши проекты</h2>
 					<div class="line"></div>
-				</div>
-				@include('includes/last_projects') -->
+				</div> -->
 			</div>
 		</div>
 		</div>
